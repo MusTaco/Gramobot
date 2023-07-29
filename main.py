303efa4d3c0b507da4140d742780cb90ad562466
MusTaco <mustafababar2003@gmail.com>
2023-07-29 17:43:32 +0500

    🍪 Login with cookies Now the program is able to log in using cookies Logs in using cookies.json file if using the program for the first time Creates cookies.json file Logs in using cookies.json every other time

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from tkinter.filedialog import askopenfilename
 import time
 import random
+import json
 
 #wait between actions(higher and random is preffered)
 rest_between_actions = random.randint(2,5)
@@ -22,24 +23,49 @@ class bot:
         driver = self.driver
         driver.get('https://instagram.com')
 
-        #Enter username
-        uname = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
-        uname.send_keys(username)
-        time.sleep(rest_between_actions)
-
-        #Enter password
-        passw = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
-        passw.send_keys(password)
-        time.sleep(rest_between_actions)
-
-        #Login
-        passw.send_keys(Keys.ENTER)
-
         try:
-            WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+            with open('cookies.json', 'r') as f:
+                cookies = json.load(f)
+            for cookie in cookies:
+                driver.add_cookie(cookie)
+            driver.refresh()
+            self.skip_not_now()
+        except FileNotFoundError:
 
+            #Enter username
+            uname = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
+            uname.send_keys(username)
+            time.sleep(rest_between_actions)
+
+            #Enter password
+            passw = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
+            passw.send_keys(password)
+            time.sleep(rest_between_actions)
+
+            #Login
+            passw.send_keys(Keys.ENTER)
+
+            self.skip_not_now()
+            try:
+                link_btns = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//a[@role="link"]')))
+                self.get_cookies()
+            except NoSuchElementException:
+                print("couldn't log in")
+        except Exception as e:
+            print(f'Error occured: {e}')
+
+    def skip_not_now(self):
+        try:
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
         except Exception:
             pass
+
+    #Get cookies
+    def get_cookies(self):
+        driver = self.driver
+        with open('cookies.json', 'w') as f:
+            json.dump(driver.get_cookies(), f)
+
 
     def instagram_post(self, post, caption):
         driver = self.driver
@@ -79,5 +105,5 @@ if __name__ == '__main__':
     file_path = askopenfilename()
     caption = 'test post'
     Bot = bot()
-    Bot.login('your_username', 'your_password')
+    Bot.login('your-username', 'your-password')
     Bot.instagram_post(file_path, caption)
